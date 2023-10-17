cee2da032dfaee3d71fba70f86fda9f4e7eadb2e
Дарьяно <dashastepanova1901@mail.ru>
2023-10-17 16:06:02 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import FirstPage from './components/FirstPage';
 import Menu from "./components/menu";
+import SecondPage from "./components/SecondPage";
+
 
 function App() {
   return (
       <div>
         <Menu/>
     <FirstPage/>
+       <SecondPage/>
       </div>
   );
 }
