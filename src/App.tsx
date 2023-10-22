5e737cb0acdd294f7f98b015b72de25289b466c0
Дарьяно <dashastepanova1901@mail.ru>
2023-10-22 23:35:08 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import FirstPage from './components/FirstPage';
 import Menu from "./components/menu";
 import SecondPage from "./components/SecondPage";
+import ThirdPage from "./components/thirdPage";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Menu/>
     <FirstPage/>
        <SecondPage/>
+          <ThirdPage/>
       </div>
   );
 }
---
+++ src/components/thirdPage.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+import './thirdpagestyle.css';
+
+const ThirdPage: FC = () => {
+    return (
+        <div id="third-page">
+            <div className="third-container">
+                <div className="title-skills">My skills</div>
+            </div>
+        </div>
+    );
+};
+
+export default ThirdPage;
